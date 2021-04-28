778295ee3b31001589f8019394052d715ce26c75
Jamie Roszel <76765652+jamieroszel@users.noreply.github.com>
2021-04-27 21:56:31 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// IMPORT useState
+import React, { useState } from "react";
+import "./styles.css";
+// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from "./imageData";
+
+export default function App() {
+  // USE useState TO CREATE  [bigImage, setBigImage]
+  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
+
+  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleClick = (imgUrl) => {
+    setBigImage(imgUrl);
+  };
+
+  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key
+  // ALSO ASSIGN AN onClick EVENT THAT CALL THE HANDLE EVENT AND PASSES IT THE IMG URL
+  const images = imagesArr.map((image, index) => {
+    return (
+      <img
+        src={image.img} //this is the image source from imageData.js
+        alt={image.city} //this is the image city from imageData.js. It's set up as the
+        className={"thumb"} //this name links to the .thumb element in styles.css. It was a relief to figure this out!
+        key={index}
+        onClick={() => handleClick(image.img)} //handleClick comes from where it was declared up above.
+      />
+    );
+  });
+
+  return (
+    <div className="App">
+      <h1>Cities Of The World</h1>
+      <div id="wrapper">
+        <div id="thumbnails">{images}</div>
+        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+        {<img src={bigImage} id="bigimage" alt="bigImage" />}
+      </div>
+    </div>
+  );
+}
